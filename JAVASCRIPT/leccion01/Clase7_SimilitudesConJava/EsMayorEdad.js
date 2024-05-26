a7484fd2308f59ca89ecb01e5077c984b56339d1
Jon@than <jony35622@gmail.com>
2024-05-26 20:19:58 -0300

    Subo cambios clase 7 javascript

---
+++ JAVASCRIPT/leccion01/Clase7_SimilitudesConJava/EsMayorEdad.js
@@ -0,0 +1,7 @@
+let edad = 20, adulto = 18;
+if( edad >= adulto){
+  console.log("Es una persona adulta");
+}
+else{
+  console.log("Es una persona menor edad");
+}
---
+++ JAVASCRIPT/leccion01/Clase7_SimilitudesConJava/OperadoresJavascript.js
@@ -0,0 +1,8 @@
+//Ejercicio para encontrar números pares
+let parImpar = 1;
+if(parImpar % 2 == 0){
+    console.log("Es un número PAR");
+}
+else{
+    console.log("Es un número IMPAR");
+}
